54eb5964cfad80d0d350f29d656835ee696c5d80
Earl John Pulido <pulidoearljohn14@gmail.com>
2022-04-01 15:51:33 +0800

    commit for earl branch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing";
 import NotFound from "./pages/notfound";
 import Andre from "./pages/andre"   //added code
+import Earl from "./pages/earl"   //added code
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/andre" element={<Andre />} />   //added code
+        <Route path="/earl" element={<Earl />} />   //added code
       </Routes>
     </Router>
   );
